bd226c9391e15c43101da8e57d01826858c710f6
145566854+VFil130@users.noreply.github.com <145566854+VFil130@users.noreply.github.com>
2025-01-31 17:12:32 +0300

    ladderv2

--- Assets/Scripts/LedderController.cs
+++ Assets/Scripts/LedderController.cs
@@ -7,15 +7,23 @@ public class LedderController : Interactor, IInteractable
     public Transform downPoint;     // Точка начала лестницы
     public Transform upPoint;       // Точка конца лестницы
     public float stepDistance = 1f; // Расстояние шага вперед после подъема
+    public Player playerController; //Ссылка на скрипт управления игроком
 
     private bool _isClimbing = false;
     [SerializeField] private Transform _player;
     [SerializeField] private Rigidbody _playerRb;
     private bool _isClimbingUp;          // Перемещается ли персонаж наверх
+
+
     public void Start()
     {
-        _playerRb=_player.GetComponent<Rigidbody>();
+        _playerRb = _player.GetComponent<Rigidbody>();
+        if (playerController == null)
+        {
+            Debug.LogError("PlayerController не назначен!");
+        }
     }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -54,6 +62,7 @@ public class LedderController : Interactor, IInteractable
         if (_isClimbingUp)
         {
             targetPosition = upPoint.position;
+            playerController.enabled = false; //Отключаем управление игроком
 
             while (Vector3.Distance(_player.position, targetPosition) > 0.1f)
             {
@@ -61,16 +70,20 @@ public class LedderController : Interactor, IInteractable
                 _player.position = Vector3.MoveTowards(_player.position, targetPosition, moveSpeed * Time.deltaTime);
                 yield return null;
             }
-            // Добавляем шаг вперед после подъема
-            Debug.Log($"Before Translate Up: _player.position: {_player.position}, transform.forward: {transform.forward}");
-            _player.Translate(transform.right * -stepDistance);
-            Debug.Log($"After Translate Up: _player.position: {_player.position}, transform.forward: {transform.forward}");
-
+            // Плавное перемещение вперед
+            Vector3 finalPosition = _player.position + transform.right * -stepDistance;
+            while (Vector3.Distance(_player.position, finalPosition) > 0.1f)
+            {
+                _player.position = Vector3.MoveTowards(_player.position, finalPosition, moveSpeed/2 * Time.deltaTime);
+                yield return null;
+            }
 
         }
         else
         {
             targetPosition = downPoint.position;
+            playerController.enabled = false; //Отключаем управление игроком
+
             while (Vector3.Distance(_player.position, targetPosition) > 0.1f)
             {
                 _player.position = Vector3.MoveTowards(_player.position, targetPosition, moveSpeed * Time.deltaTime);
@@ -80,13 +93,14 @@ public class LedderController : Interactor, IInteractable
         _isClimbing = false;
         _isClimbingUp = false;
         _playerRb.useGravity = true;
+        playerController.enabled = true; //Включаем управление игроком
     }
     public void Interact()
     {
         Debug.Log("Interact called");
 
         // Поворот персонажа к лестнице только по оси Y
-        Vector3 forwardDirection = transform.forward;
+        Vector3 forwardDirection = -transform.right;
         forwardDirection.y = 0; // Игнорируем Y-компоненту
         _player.rotation = Quaternion.LookRotation(forwardDirection);
         StartCoroutine(ClimbCoroutine());
