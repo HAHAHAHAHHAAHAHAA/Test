547bb865f19247b9c1cf2e397be1937631c14d9b
145566854+VFil130@users.noreply.github.com <145566854+VFil130@users.noreply.github.com>
2025-04-13 01:32:55 +0300

    d

--- Assets/Scripts/PickItem.cs
+++ Assets/Scripts/PickItem.cs
@@ -9,10 +9,13 @@ public class PickItem : Interactor, IInteractable
     [SerializeField] private int shotgunAmmo;
     [SerializeField] private int submachinegunAmmo;
     [SerializeField] private int rifleAmmo;
+    [SerializeField] private AudioSource Player;
+    [SerializeField] AudioClip pickupSound;
     public void Interact()
     {
         print("Item picked");
-        RecourceGet(money,metal,cloth,pistolAmmo,shotgunAmmo,submachinegunAmmo,rifleAmmo);
+        RecourceGet(money, metal, cloth, pistolAmmo, shotgunAmmo, submachinegunAmmo, rifleAmmo);
+        Player.PlayOneShot(pickupSound);
         Destroy(this.gameObject);
     }
 }
