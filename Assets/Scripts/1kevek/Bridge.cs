32609078ae5cf4cc2ab34f6dc55e55be3887763b
HAHAHAHAHA <nikita.cher24102002@mail.ru>
2025-05-10 12:11:08 +0300

    sounds and colliders

--- Assets/Scripts/1kevek/Bridge.cs
+++ Assets/Scripts/1kevek/Bridge.cs
@@ -5,6 +5,11 @@ using System.Collections;
 
 public class Bridge : BaseAction
 {
+    private bool hasPlayedClip1 = false;
+    private bool hasPlayedClip2 = false;
+    public AudioSource m_AudioSource;
+    public AudioClip m_Clip;
+    public AudioClip m_Clip2;
     public GameObject bridgeControl1, bridgeControl2;
     public GameObject idiotControll;
     public float rotationSpeed = 2f;
@@ -18,24 +23,46 @@ public class Bridge : BaseAction
 
     private void FixedUpdate()
     {
+        // Сброс флагов, если состояние изменилось
+        if (count != 1) hasPlayedClip1 = false;
+        if (count != 2) hasPlayedClip2 = false;
+
         if (count == 1)
         {
             float currentAngle = transform.eulerAngles.x;
             float newAngle = Mathf.LerpAngle(currentAngle, -25, rotationSpeed * Time.deltaTime);
             transform.eulerAngles = new Vector3(newAngle, transform.eulerAngles.y, transform.eulerAngles.z);
+
+            // Проигрываем звук только один раз при входе в count == 1
+            if (!hasPlayedClip1)
+            {
+                m_AudioSource.clip = m_Clip;
+                m_AudioSource.Play();
+                hasPlayedClip1 = true;
+            }
         }
+
         if (count == 2)
         {
             if (!isCoroutineStarted)
             {
-                StartCoroutine(RebuildNavMeshWithDelay()); // Запускаем корутину
+                StartCoroutine(RebuildNavMeshWithDelay());
                 isCoroutineStarted = true;
             }
+
             float currentAngle = transform.eulerAngles.x;
             float newAngle = Mathf.LerpAngle(currentAngle, -111f, rotationSpeed * Time.deltaTime);
             transform.eulerAngles = new Vector3(newAngle, transform.eulerAngles.y, transform.eulerAngles.z);
-            StartCoroutine(Col());
 
+            // Проигрываем звук только один раз при входе в count == 2
+            if (!hasPlayedClip2)
+            {
+                m_AudioSource.clip = m_Clip2;
+                m_AudioSource.Play();
+                hasPlayedClip2 = true;
+            }
+
+            StartCoroutine(Col());
         }
     }
     IEnumerator Col()
